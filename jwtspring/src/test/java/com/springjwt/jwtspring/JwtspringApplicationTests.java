200ecd7ca101e162d78afcc4ce032ca65023a440
cindoddcindy <cindodcindy@gmail.com>
2022-12-21 15:14:48 +0700

    add maven package

---
+++ jwtspring/src/test/java/com/springjwt/jwtspring/JwtspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjwt.jwtspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
